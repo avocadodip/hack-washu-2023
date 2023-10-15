206f3685fee658beb83396faf6192ae200192d7e
cewidiupleek <56790819+cewidiupleek@users.noreply.github.com>
2023-10-15 11:57:59 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Form from "../src/components/Form";
 import World from "../src/components/World";
+<<<<<<< Updated upstream
+=======
+import "./App.css";
+>>>>>>> Stashed changes
 
 const App = () => {
   const [html, setHtml] = useState("");
   const [input, setInput] = useState("");
+<<<<<<< Updated upstream
 
   // Generates HTML
   const handleSubmit = async () => {
@@ -12,13 +17,68 @@ const App = () => {
     const API_KEY = process.env.REACT_APP_OPEN_AI;
 
     const parameters = `Generate some random elements with random background color. Always generate ground and sky. Only use native a-frames. The scene should follow this description? ${input}. Generate 20 a-frames. Unless otherwise stated, the world should be naturalistic (trees, river, etc.)`;
+=======
+  const [loading, setLoading] = useState(false);
+
+  // Generates HTML
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setLoading(true);
+
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const API_KEY = process.env.REACT_APP_OPEN_AI;
+
+    const userDescription =
+      "You are an A-Frame expert. Your task is to create an A-Frame scene that adheres to the user's specifications exactly. \
+          Utilize native A-Frame elements, such as cylinders, cones, boxes, rectangles, and others, to represent naturalistic elements defined below. \
+          Your role is to generate these elements exactly based on the user's input and the parameters given about generating proper scene objects and color selection. \
+          Surround your response with <a-scene> tags for proper A-Frame HTML generation.";
+
+    const parameters = `Given the user's desire for a ${input}, generate a custom A-Frame scene using one of the preset environments. Choose a preset that best matches the user's description and adjust the dressingAmount to suit the theme. The presets available are: 'forest', 'egypt', 'checkerboard', 'goaland', 'yavapai', 'goldmine', 'arches', 'threetowers', 'poison', 'tron', 'japan', 'dream', 'volcano', 'starry', 'osiris'. 
+
+          If the user prompts in two or more presets out of the ones available: 'forest', 'egypt', 'checkerboard', 'goaland', 'yavapai', 'goldmine', 'arches', 'threetowers', 'poison', 'tron', 'japan', 'dream', 'volcano', 'starry', 'osiris', then generate a custom A-Frame scene using the set amount requested by the user based on the preset environments available. For example, if the user requests ‘japan’ and ‘forest’ the scene that is built should only contain the presets for ‘japan’ and ‘forest’. Choose presets that best matches the user's description and adjust the dressingAmount to suit the theme.
+          
+              - Be very precise when it comes to dressingAmount, as it depends on the preset. Be realistic to the real world. For example, 'japan' has pillars, and there should be less of these, like 1 or 2. For 'forest', there should be way more (as there are hundreds of trees in a forest). To provide more context, for larger presets like egypt or arches, the dressingAmount should be around 100 because. For smaller generations like forest, the dressingAmount should be around 350. 
+              
+              Provide the A-Frame scene code below:
+          
+              For the 'fog' attribute, you only have the following options to choose from:
+              - any decimal number from 0.0 to 0.8. Cannot be larger than 0.8. 
+          
+              For the 'groundTexture' attribute, you only have the following options to choose from:
+              - 'checkerboard'
+              - 'squares'
+              - 'walkernoise'
+              
+              - If the user types in night, dark, scary, horror, nightmare, or any similar words, only use these following colors for 'skyColor' and 'horizonColor' attributes: 
+              - skyColor: #05060B, #0e0b07, #000000, 
+              - horizonColor: #caf0fe
+          
+              - only modify the presets; don't add new lines
+          
+              <a-scene inspector="" keyboard-shortcuts="" screenshot="" vr-mode-ui="" device-orientation-permission-ui=""
+              fog="color: [HEX_CODE]; far: [VALUE]; density: [VALUE]" visible=""
+              environment="active: true; preset: [PRESET_SELECTION]; skyType: gradient; skyColor: [SKY COLOR THAT MATCHES PRESET]; 
+              horizonColor: [COLOR THAT MATCHES PRESET AND IS A HORIZON COLOR]; fog: [VALUE]; ground: [SELECTION THAT MATCHES PRESET]; groundTexture: [SELECTION THAT MATCHES PRESET]; 
+              groundColor: [GROUND COLOR THAT IS IN A SIMILAR COLOR PALETTE TO PRESET OR IS REALISTIC TO WHAT WE CAN SEE IN REAL LIFE]; groundColor2: [ANOTHER GROUND COLOR THAT IS IN A SIMILAR COLOR PALETTE TO PRESET OR IS REALISTIC TO WHAT WE CAN SEE IN REAL LIFE]
+              camera="">
+                  <!-- Generated custom environment! -->
+                  <a-entity environment="preset: [PRESET-SELECTION]; dressingAmount: [DRESSING-AMOUNT]"></a-entity>
+              </a-scene>
+                    `;
+>>>>>>> Stashed changes
 
     // Construct object
     const messages = [
       {
         role: "system",
+<<<<<<< Updated upstream
         content:
           "You are an a-frame HTML generator. Surround your response with <a-scene> tags.",
+=======
+        content: userDescription,
+>>>>>>> Stashed changes
       },
       {
         role: "user",
@@ -41,17 +101,15 @@ const App = () => {
     });
 
     const data = await response.json();
-
-    console.log("1");
-    console.log(data);
-
     const generatedHtml = data.choices?.[0]?.message?.content || "";
-
-    console.log("2");
-    console.log(generatedHtml);
     setHtml(generatedHtml);
+    setLoading(false);
+
+    console.log("HTML FROM GPT BELOW:");
+    console.log(generatedHtml);
   };
 
+<<<<<<< Updated upstream
   const handleInputChange = (newValue) => {
     setInput(newValue);
   };
@@ -66,6 +124,21 @@ const App = () => {
       />
       <World html={html} />
     </div>
+=======
+  return (
+    <>
+      <div className="App">
+        <Form
+          className="form-style"
+          loading={loading}
+          input={input}
+          setInput={setInput}
+          handleSubmit={handleSubmit}
+        />
+        <World html={html} />
+      </div>
+    </>
+>>>>>>> Stashed changes
   );
 };
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { useState } from 'react';
 
 function Form({ onSubmit, onInputChange, input }) {
@@ -6,10 +7,23 @@ function Form({ onSubmit, onInputChange, input }) {
   const handleSubmit = (event) => {
     event.preventDefault();
     onSubmit(parameters);
+=======
+import React from 'react';
+import '../App.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import CircularProgress from '@mui/material/CircularProgress';
+
+function Form({ handleSubmit, loading, input, setInput }) {
+
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+>>>>>>> Stashed changes
   };
 
   return (
     <form onSubmit={handleSubmit} className="form-style">
+<<<<<<< Updated upstream
       <input
         type="text"
         value={input}
@@ -17,6 +31,14 @@ function Form({ onSubmit, onInputChange, input }) {
         placeholder="Describe your world..."
       />
       <button type="submit">Generate</button>
+=======
+      <TextField id="filled-basic" label="Design your world..." variant="filled" type="text"
+        value={input}
+        onChange={handleInputChange}/>
+      <Button type="submit" variant="contained" style={{height: '40px'}} disabled={loading || !input.trim()}>
+        {loading ? <CircularProgress size={20} style={{color: 'white', marginLeft: '10px'}} /> : 'Generate'}
+      </Button>
+>>>>>>> Stashed changes
     </form>
   );
 }

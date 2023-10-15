bf2243480851fadebf9e941882e5cfc170242d99
cewidiupleek <56790819+cewidiupleek@users.noreply.github.com>
2023-10-15 12:44:31 -0500

    push

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,8 @@ import '../App.css';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import CircularProgress from '@mui/material/CircularProgress';
+import Modal from './Modal';
+import Grid from '@mui/material/Grid';
 
 function Form({ handleSubmit, loading, input, setInput }) {
 
@@ -11,14 +13,39 @@ function Form({ handleSubmit, loading, input, setInput }) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="form-style">
-      <TextField id="filled-basic" label="Design your world..." variant="filled" type="text"
-        value={input}
-        onChange={handleInputChange}/>
-      <Button type="submit" variant="contained" style={{height: '40px'}} disabled={loading || !input.trim()}>
-        {loading ? <CircularProgress size={20} style={{color: 'white', marginLeft: '10px'}} /> : 'Generate'}
-      </Button>
-    </form>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <form onSubmit={handleSubmit} className="form-style">
+          <Grid container spacing={0}>
+            <Grid item xs={11}>
+              <TextField 
+                id="filled-basic" 
+                label="Design your world..." 
+                variant="filled" 
+                type="text"
+                value={input}
+                onChange={handleInputChange}
+                InputLabelProps={{
+                  style: { color: '#FFFFFF', opacity: 0.6 },
+                }}
+                InputProps={{
+                  style: { color: '#FFFFFF' },
+                }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={1} style={{ display: 'flex', alignItems: 'flex-end' }}>
+              <Modal />
+            </Grid>
+            <Grid item xs={12}>
+              <Button type="submit" variant="contained" style={{height: '40px'}} disabled={loading || !input.trim()}>
+                {loading ? <CircularProgress size={20} style={{color: 'white', marginLeft: '10px'}} /> : 'Generate'}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
   );
 }
 
---
+++ src/components/Modal.jsx
@@ -0,0 +1,111 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import Grid from '@mui/material/Grid';
+import PublicIcon from '@mui/icons-material/Public';
+import "../App.css";
+
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+export default function Modal() {
+    const [open, setOpen] = React.useState(false);
+  
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+    
+    return (
+      <div>
+
+    <IconButton
+        size="large"
+        aria-label="help"
+        color="inherit"
+        sx={{ mx: 1 }}
+        onClick={handleClickOpen}
+      >
+        <HelpOutlineIcon />
+      </IconButton>
+        
+        <BootstrapDialog
+          onClose={handleClose}
+          aria-labelledby="customized-dialog-title"
+          open={open}
+        >
+
+<Grid container alignItems="center" sx={{py:1}}>
+      <Grid item sx={{ ml: 2}}>
+        <PublicIcon  color="inherit" sx={{ mt: 0.5 }} />
+      </Grid>
+      <Grid item>
+        <DialogTitle className="bolder-blue" sx={{ mt: 0, p: 0.5 }} id="customized-dialog-title">
+          BLDR
+        </DialogTitle>
+      </Grid>
+      <Grid className="blue" item sx={{ marginLeft: 'auto', mr: 2}}>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      </Grid>
+    </Grid>
+          
+          
+          <DialogContent dividers>
+            <Typography gutterBottom sx={{mb:3}}> 
+              Welcome to Wrld Bldr, <span className="blue">your personalized 3D world builder!</span>
+            </Typography>
+            <Typography gutterBottom sx={{mb:3}}>
+              Use the text field below to enter in your preferences for your world. Feel free to use our recommendations or experiment with your own. Explore your world using your mouse/trackpad and arrow keys.
+              Try typing these:
+               forest,
+                        pyramid,
+                        checkerboard,
+                      goaland,
+                       yavapai,
+                         goldmine,
+                      arches,
+                      threetowers,
+                       poison,
+                        tron,
+                      japan,
+                      dream,
+                       volcano,
+                        starry,
+                        osiris.
+            </Typography>
+            <Typography gutterBottom >
+              Happy creating, we hope you enjoy!
+            </Typography>
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={handleClose}>
+              Got it!
+            </Button>
+          </DialogActions>
+        </BootstrapDialog>
+      </div>
+    );
+  }
